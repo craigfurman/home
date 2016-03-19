98c03380c18219330e771f888545b8a028856e4d
Craig Furman <craig.furman89@gmail.com>
2016-03-19 13:27:58 +0000

    lower high-DPI mouse sensitivity

--- unity-startup-scripts/reduce-mouse-sensitivity.sh
+++ unity-startup-scripts/reduce-mouse-sensitivity.sh
@@ -5,7 +5,7 @@ mouse_name="Razer Razer Abyssus"
 if xinput --list --short | grep "$mouse_name" > /dev/null
 then
   echo "Razer mouse detected: reducing sensitivity"
-  xinput --set-prop "$mouse_name" "Device Accel Constant Deceleration" 1.5
+  xinput --set-prop "$mouse_name" "Device Accel Constant Deceleration" 1.94
   xinput --set-prop "$mouse_name" "Device Accel Velocity Scaling" 1
 else
   echo "no Razer mouse detected: not doing anything"
